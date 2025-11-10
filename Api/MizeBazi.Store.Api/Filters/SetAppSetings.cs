0ec90af6ca8fff6ef13f2e4b95d44d8237ad4277
kmd66 <105439395+kmd66@users.noreply.github.com>
2025-11-10 08:27:47 +0100

    add AppSetings

---
+++ Api/MizeBazi.Store.Api/Filters/SetAppSetings.cs
@@ -0,0 +1,20 @@
+﻿
+using MizeBazi.Store.Common.Helper;
+
+namespace MizeBazi.Store.Api.Filters;
+public static class ConfigurationRegistration
+{
+    public static IConfiguration SetAppSetings(this IConfiguration config)
+    {
+        AppSetings.WriteConnection = config.GetSection("ConnectionStrings:WriteConnection").Value;
+        AppSetings.ReadConnection = config.GetSection("ConnectionStrings:ReadConnection").Value;
+
+        AppSetings.JwtKey = config.GetSection("Jwt:Key").Value!;
+        AppSetings.JwtIv = config.GetSection("Jwt:Iv").Value!;
+        AppSetings.AccessTokenTime = int.Parse(config.GetSection("Jwt:AccessTokenTime").Value!);
+
+
+        return config;
+    }
+
+}
---
+++ Core/MizeBazi.Store.Common/Helper/AppSetings.cs
@@ -0,0 +1,11 @@
+﻿
+namespace MizeBazi.Store.Common.Helper;
+public static class AppSetings
+{
+    public static string WriteConnection { get; set; }
+    public static string ReadConnection { get; set; }
+    public static string JwtKey { get; set; }
+    public static string JwtIv { get; set; }
+    public static int AccessTokenTime { get; set; }
+
+}
