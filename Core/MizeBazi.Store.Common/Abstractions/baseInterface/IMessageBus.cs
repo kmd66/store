0ec8dc2c33d05d236288af699c2af1f3d7d748f8
kmd66 <105439395+kmd66@users.noreply.github.com>
2025-11-11 17:32:57 +0100

    IMessageBus ad

---
+++ Core/MizeBazi.Store.Common/Abstractions/baseInterface/IMessageBus.cs
@@ -0,0 +1,11 @@
+﻿
+using MizeBazi.Store.Common.Shared;
+
+namespace MizeBazi.Store.Common.Abstractions;
+
+public interface IMessageBus
+{
+    Task PullAsync(DataSyncEvent model);
+   
+    Task PushAsync(DataSyncEvent model);
+}
---
+++ Infrastructure/MizeBazi.Store.Services/RabbitMQMessageBus.cs
@@ -0,0 +1,18 @@
+﻿using MizeBazi.Store.Common.Shared;
+using MizeBazi.Store.Common.Helper;
+using MizeBazi.Store.Common.Abstractions;
+
+namespace MizeBazi.Store.Services;
+
+public class RabbitMQMessageBus : IMessageBus
+{
+    public Task PullAsync(DataSyncEvent model)
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task PushAsync(DataSyncEvent model)
+    {
+        return Task.CompletedTask;
+    }
+}
