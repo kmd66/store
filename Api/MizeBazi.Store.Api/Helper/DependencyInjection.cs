db2257335de57ef9e1d9f5ca5cf110dce6b4a4a5
kmd66 <105439395+kmd66@users.noreply.github.com>
2025-11-12 18:15:55 +0100

    di fix

--- Api/MizeBazi.Store.Api/Helper/DependencyInjection.cs
+++ Api/MizeBazi.Store.Api/Helper/DependencyInjection.cs
@@ -15,12 +15,10 @@ public static class DependencyInjection
     {
         EfDependency(services);
 
-
-
         // ثبت MediatR فقط در WebAPI
-        var appAssembly = Assembly.Load("MizeBazi.Store.Application"); 
+        var assemblyApplication = Assembly.Load("MizeBazi.Store.Application"); 
         services.Scan(scan => scan
-            .FromAssemblies(appAssembly)
+            .FromAssemblies(assemblyApplication)
             .AddClasses(classes => classes.AssignableTo(typeof(ICommandHandler<,>)))
                 .AsImplementedInterfaces()
                 .WithScopedLifetime()
@@ -30,14 +28,21 @@ public static class DependencyInjection
             .AddClasses(classes => classes.AssignableTo(typeof(IBehaviorHandler<>)))
                 .AsImplementedInterfaces()
                 .WithScopedLifetime()
+            .AddClasses(classes => classes.AssignableTo(typeof(IDomainEventHandler<>)))
+                .AsImplementedInterfaces()
+                .WithScopedLifetime()
         );
+        servicesScanWithName(services, assemblyApplication);
+        servicesScanWithName(services, Assembly.Load("MizeBazi.Store.Common"));
+        servicesScanWithName(services, Assembly.Load("MizeBazi.Store.Domain"));
+
+        servicesScanWithName(services, Assembly.Load("MizeBazi.Store.Data"));
+        //servicesScanWithName(services, Assembly.Load("MizeBazi.Store.Extention"));
+        servicesScanWithName(services, Assembly.Load("MizeBazi.Store.Services"));
+
+        servicesScanWithName(services, Assembly.Load("MizeBazi.Store.Api"));
 
         services.AddScoped<IAppMediator, MediatorAdapter>();
-
-
-        services.AddTransient<IDomainEventHandler<OrderConfirmedEvent>, OrderConfirmedEventHandler>();
-
-        services.AddScoped<IEventDispatcher, EventDispatcher>();
         services.AddScoped<AutoEventDispatcher>();
 
         services.AddSingleton<IMessageBus, RabbitMQMessageBus>();
@@ -58,5 +63,19 @@ public static class DependencyInjection
             options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
             options.AddInterceptors(sp.GetRequiredService<AppSaveChanges>());
         });
+    }
+    private static void servicesScanWithName(IServiceCollection services, Assembly ass)
+    {
+        services.Scan(scan => scan
+            .FromAssemblies(ass)
+            .AddClasses(classes => classes.Where(c =>
+                c.IsClass &&
+                !c.IsAbstract &&
+                c.GetInterfaces().Any(i =>
+                    i.Name.StartsWith("I") &&
+                    i.Name.Substring(1) == c.Name)))
+                .AsImplementedInterfaces()
+                .WithScopedLifetime()
+        );
     }
 }
--- Api/MizeBazi.Store.Api/Program.cs
+++ Api/MizeBazi.Store.Api/Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddHttpContextAccessor();
 builder.Configuration.SetAppSetings();
 builder.Services.AddServices();
 
