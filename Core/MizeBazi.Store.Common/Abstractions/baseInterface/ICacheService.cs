425a54e49fdf76f629da4590e727a9e39a1cb765
kmd66 <105439395+kmd66@users.noreply.github.com>
2025-11-13 15:00:13 +0100

    Memory Cache add

---
+++ Core/MizeBazi.Store.Common/Abstractions/baseInterface/ICacheService.cs
@@ -0,0 +1,11 @@
+﻿namespace MizeBazi.Store.Common.Abstractions;
+public interface ICacheService
+{
+    T Get<T>(string group, Guid key);
+    void Set<T>(string group, Guid key, T value, int slidingExpiry = 0, int absoluteExpiry = 0);
+    bool TryGetValue<T>(string group, Guid key, out T value);
+    void Remove(string key);
+    void Remove(string group, Guid key);
+    bool Exists(string group, Guid key);
+    IEnumerable<KeyValuePair<string, T>> SearchByPartition<T>(string partition, Func<T, bool> valueFilter = null);
+}
---
+++ Core/MizeBazi.Store.Common/Shared/models/Cache.cs
@@ -0,0 +1,2 @@
+﻿namespace MizeBazi.Store.Common.Shared;
+public record CacheInMemoryRecord(Guid Id, Guid TokenId, long UserId, UserRoles Role);
